9eeb5142f7e2869b84fc36d43c31f035f4e68333
Sven Mol <s.mol@student.utwente.nl>
2020-07-02 17:36:05 +0200

    Fix failing tests
    Should've done this sooner

--- tests/AssignmentSolverTest.php
+++ tests/AssignmentSolverTest.php
@@ -6,9 +6,10 @@ namespace Kickin\Hungarian\Tests;
 use Exception;
 use Kickin\Hungarian\Algo\AssignmentSolver;
 use Kickin\Hungarian\Algo\Hungarian;
-use Kickin\Hungarian\Algo\JonkerVolgenant;
+use Kickin\Hungarian\Matrix\LabeledMatrix;
 use Kickin\Hungarian\Matrix\Matrix;
 use PHPUnit\Framework\TestCase;
+use stdClass;
 
 class AssignmentSolverTest extends TestCase
 {
@@ -21,7 +22,6 @@ class AssignmentSolverTest extends TestCase
 	{
 		return [
 			[new Hungarian()],
-			[new JonkerVolgenant()],
 		];
 	}
 
@@ -71,41 +71,85 @@ class AssignmentSolverTest extends TestCase
 	public function testSolveComplex(AssignmentSolver $instance)
 	{
 		$matrix = new Matrix(5);
-		$matrix->set(0,0,0);
-		$matrix->set(0,1,1);
-		$matrix->set(0,2,2);
-		$matrix->set(0,3,3);
-		$matrix->set(0,4,4);
-		$matrix->set(1,0,3);
-		$matrix->set(1,1,2);
-		$matrix->set(1,2,1);
-		$matrix->set(1,3,0);
-		$matrix->set(1,4,1);
-		$matrix->set(2,0,0);
-		$matrix->set(2,1,1);
-		$matrix->set(2,2,2);
-		$matrix->set(2,3,3);
-		$matrix->set(2,4,4);
-		$matrix->set(3,0,1);
-		$matrix->set(3,1,2);
-		$matrix->set(3,2,0);
-		$matrix->set(3,3,1);
-		$matrix->set(3,4,2);
-		$matrix->set(4,0,1);
-		$matrix->set(4,1,0);
-		$matrix->set(4,2,3);
-		$matrix->set(4,3,1);
-		$matrix->set(4,4,2);
+		$matrix->set(0, 0, 0);
+		$matrix->set(0, 1, 1);
+		$matrix->set(0, 2, 2);
+		$matrix->set(0, 3, 3);
+		$matrix->set(0, 4, 4);
+		$matrix->set(1, 0, 3);
+		$matrix->set(1, 1, 2);
+		$matrix->set(1, 2, 1);
+		$matrix->set(1, 3, 0);
+		$matrix->set(1, 4, 1);
+		$matrix->set(2, 0, 0);
+		$matrix->set(2, 1, 1);
+		$matrix->set(2, 2, 2);
+		$matrix->set(2, 3, 3);
+		$matrix->set(2, 4, 4);
+		$matrix->set(3, 0, 1);
+		$matrix->set(3, 1, 2);
+		$matrix->set(3, 2, 0);
+		$matrix->set(3, 3, 1);
+		$matrix->set(3, 4, 2);
+		$matrix->set(4, 0, 1);
+		$matrix->set(4, 1, 0);
+		$matrix->set(4, 2, 3);
+		$matrix->set(4, 3, 1);
+		$matrix->set(4, 4, 2);
 
 		$result = $instance->solve($matrix);
+
+		self::assertEquals(3, $result->getCost($matrix));
 
 		self::assertEquals(0, $result->getCol(1));
 		self::assertEquals(1, $result->getCol(4));
 		self::assertEquals(2, $result->getCol(0));
 		self::assertEquals(3, $result->getCol(2));
 		self::assertEquals(4, $result->getCol(3));
+	}
 
-		self::assertEquals(3, $result->getCost($matrix));
+	/**
+	 * @dataProvider instances
+	 * @param AssignmentSolver $solver
+	 * @throws Exception
+	 */
+	public function testSolveTShaped(AssignmentSolver $solver)
+	{
+		$matrix = new Matrix(5);
+		$matrix->set(0, 2, 0);
+		$matrix->set(1, 3, 0);
+		$matrix->set(2, 4, 0);
+		$matrix->set(3, 1, 0);
+		$matrix->set(4, 0, 0);
+
+		$result = $solver->solve($matrix);
+
+		self::assertEquals(2, $result->getRow(0));
+		self::assertEquals(3, $result->getRow(1));
+		self::assertEquals(4, $result->getRow(2));
+		self::assertEquals(1, $result->getRow(3));
+		self::assertEquals(0, $result->getRow(4));
+	}
+
+	/**
+	 * @dataProvider instances
+	 * @param AssignmentSolver $solver
+	 * @throws Exception
+	 */
+	public function testSolveLabeled(AssignmentSolver $solver)
+	{
+		$rows = [new stdClass(), new stdClass(), new stdClass()];
+		$cols = [new stdClass(), new stdClass(), new stdClass()];
+		$matrix = new LabeledMatrix($rows, $cols);
+		$matrix->set($rows[0], $cols[1], 0);
+		$matrix->set($rows[1], $cols[2], 0);
+		$matrix->set($rows[2], $cols[0], 0);
+
+		$result = $solver->solve($matrix);
+
+		self::assertEquals($cols[1], $result->getRow($rows[0]));
+		self::assertEquals($cols[2], $result->getRow($rows[1]));
+		self::assertEquals($cols[0], $result->getRow($rows[2]));
 	}
 
 	public function testEquivalence()
